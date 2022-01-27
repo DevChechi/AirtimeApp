2320264d8860c70b12f2dac04b2844492d279b23
Perpetua-Kabute <kabuteperpetua@gmail.com>
2022-01-27 16:28:15 +0300

    chore: move main activity to activites package

--- app/src/main/java/com/perpetua/eazytopup/activities/MainActivity.kt
+++ app/src/main/java/com/perpetua/eazytopup/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.perpetua.eazytopup
+package com.perpetua.eazytopup.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.perpetua.eazytopup.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
