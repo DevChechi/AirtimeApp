d5b2ca69cd3db2ff7776e5d3745f16408e61e1ad
Perpetua-Kabute <kabuteperpetua@gmail.com>
2022-02-02 06:32:45 +0300

    fix: add clear full screen flag

--- app/src/main/java/com/perpetua/eazytopup/activities/MainActivity.kt
+++ app/src/main/java/com/perpetua/eazytopup/activities/MainActivity.kt
@@ -3,11 +3,13 @@ package com.perpetua.eazytopup.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import com.perpetua.eazytopup.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         val decorView: View = window.decorView
         val uiOptions: Int = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         decorView.setSystemUiVisibility(uiOptions)
