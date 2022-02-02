8a9df6cdaf68dc6ca0f73f28507f007cb1789f1a
Perpetua-Kabute <kabuteperpetua@gmail.com>
2022-02-02 06:30:12 +0300

    fix: add status bar

--- app/src/main/java/com/perpetua/eazytopup/activities/MainActivity.kt
+++ app/src/main/java/com/perpetua/eazytopup/activities/MainActivity.kt
@@ -2,11 +2,16 @@ package com.perpetua.eazytopup.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.perpetua.eazytopup.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val decorView: View = window.decorView
+        val uiOptions: Int = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        decorView.setSystemUiVisibility(uiOptions)
+        decorView.setBackgroundColor(resources.getColor(android.R.color.white))
         setContentView(R.layout.activity_main)
     }
 }
