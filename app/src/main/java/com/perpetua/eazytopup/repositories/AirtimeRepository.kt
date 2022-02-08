c9d409f80ac43f048ab12125f6347b6091dc465c
Perpetua-Kabute <kabuteperpetua@gmail.com>
2022-02-08 21:01:11 +0300

    chore: configure repository

---
+++ app/src/main/java/com/perpetua/eazytopup/repositories/AirtimeRepository.kt
@@ -0,0 +1,15 @@
+package com.perpetua.eazytopup.repositories
+
+import com.perpetua.eazytopup.apis.RetrofitInstance
+import com.perpetua.eazytopup.models.AirtimeForOther
+import com.perpetua.eazytopup.models.AirtimeForSelf
+
+class AirtimeRepository {
+    suspend fun buyAirtimeForSelf(airtimeForSelf: AirtimeForSelf){
+        RetrofitInstance.api.buyAirtimeForSelf(airtimeForSelf)
+    }
+
+    suspend fun buyAirtimeForOther(airtimeForOther: AirtimeForOther){
+        RetrofitInstance.api.buyAirtimeForOther(airtimeForOther)
+    }
+}
