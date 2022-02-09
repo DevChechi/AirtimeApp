56501c48522ef03b2142c74b5165601a1b401a83
Perpetua-Kabute <kabuteperpetua@gmail.com>
2022-02-09 14:34:07 +0300

    feat: make network call to buy airtime for self

---
+++ app/src/main/java/com/perpetua/eazytopup/viewmodels/AirtimeViewModel.kt
@@ -0,0 +1,43 @@
+package com.perpetua.eazytopup.viewmodels
+
+import android.util.Log.d
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.perpetua.eazytopup.models.AirtimeForOther
+import com.perpetua.eazytopup.models.AirtimeForSelf
+import com.perpetua.eazytopup.repositories.AirtimeRepository
+import com.perpetua.eazytopup.utils.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.awaitResponse
+
+class AirtimeViewModel(private val airtimeRepository: AirtimeRepository) : ViewModel() {
+
+    val airtimeForSelfData: MutableLiveData<Resource<Unit>> = MutableLiveData()
+    init{
+        d("AirtimeViewModel", "View model started")
+    }
+    fun buyAirtimeForSelf(airtimeForSelf: AirtimeForSelf) = viewModelScope.launch(Dispatchers.IO) {
+        airtimeForSelfData.postValue(Resource.Loading())
+        if(airtimeForSelf.number.isEmpty()){
+            airtimeForSelfData.postValue(Resource.PhoneNumberError("Phone Number missing"))
+        }
+        if(airtimeForSelf.pesa.isEmpty()){
+            airtimeForSelfData.postValue(Resource.AmountError("Amount missing"))
+        }
+        val response = airtimeRepository.buyAirtimeForSelf(airtimeForSelf).awaitResponse()
+        handleAirtimeResponse(response.body())
+       }
+
+
+    private fun handleAirtimeResponse(response: Response<Unit>?) : Resource<String>{
+        when(response?.code()){
+            200 -> {return Resource.Success("Successful, Wait for Mpesa Pin prompt")}
+            404 -> {return Resource.Error("Error, Request failed")}
+            500 -> {return Resource.Error("Server error: Try again later")}
+        }
+        return  Resource.Error("Error, Request failed")
+    }
+}
