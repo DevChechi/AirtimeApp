d09a276c38ca0c220f401f6ff68ac363de9eed9f
Perpetua-Kabute <kabuteperpetua@gmail.com>
2022-02-10 15:12:53 +0300

    fix: right base url

--- app/src/main/java/com/perpetua/eazytopup/utils/Constants.kt
+++ app/src/main/java/com/perpetua/eazytopup/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.perpetua.eazytopup.utils
 
 class Constants {
     companion object{
-        const val BASE_URL = "https://164.90.133.19:4040"
+        const val BASE_URL = "http://164.90.133.19:2623"
     }
 }
