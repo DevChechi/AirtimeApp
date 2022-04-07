11d7a5a223a9bfdcab78d03fbea020e668e94e9e
AmosNyaundi <amosnyaundi4@gmail.com>
2022-04-07 11:49:09 +0300

    url fix

--- app/src/main/java/com/perpetua/eazytopup/utils/Constants.kt
+++ app/src/main/java/com/perpetua/eazytopup/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.perpetua.eazytopup.utils
 
 class Constants {
     companion object{
-        const val BASE_URL = "http://164.90.133.19:2623"
+        const val BASE_URL = "https://portal.eazytopup.co.ke:4040"
     }
 }
